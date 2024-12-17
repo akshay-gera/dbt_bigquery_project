a45a0321ebf28e5dd1bf9a67c6ddc34e2b535149
Akshay Gera <98545133+akshay-gera@users.noreply.github.com>
2024-12-17 09:23:37 +0100

    Joined Dim Hosts with Listings

---
+++ models/dim/dim_listings_with_hosts.sql
@@ -0,0 +1,41 @@
+WITH hosts AS(
+    SELECT
+        host_id,
+        host_name,
+        is_superhost,
+        created_at AS host_created_at,
+        updated_at AS host_updated_at
+    FROM 
+        {{ ref('dim_hosts') }}
+),
+listings AS(
+    SELECT
+        listing_id,
+        listing_name,
+        listing_room_type,
+        minimum_nights,
+        price,
+        created_at AS listing_created_at,
+        updated_at AS listing_updated_at,
+        host_id
+    FROM
+        {{ ref('dim_listings_cleansed') }}
+)
+SELECT
+    l.listing_id,
+    l.listing_name,
+    l.listing_room_type,
+    l.minimum_nights,
+    l.price,
+    l.listing_created_at,
+    l.listing_updated_at,
+    l.host_id,
+    h.host_name,
+    h.is_superhost,
+    h.host_created_at,
+    h.host_updated_at
+FROM
+    listings AS l
+LEFT JOIN
+    hosts AS h
+ON l.host_id = h.host_id
